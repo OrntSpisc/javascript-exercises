03a8a7b7d30b1bdeceb6be77858eaf63b1b5e585
UdYnamite <89peterj89@gmail.com>
2023-07-14 12:48:26 +0700

    03_reverseString Done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let result = '';
+    let array = string.split('');
+    for (let i = array.length - 1; i >= 0; i--) {
+        result += array[i];
+    }
+    return result;
 };
 
 // Do not edit below this line
