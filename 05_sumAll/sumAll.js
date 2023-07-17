062760a4ef84e619607274c6ea17ae6a120ec91b
UdYnamite <89peterj89@gmail.com>
2023-07-17 13:31:18 +0700

    05_sumAll Done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
+const sumAll = function(number1, number2) {
+    if(number1 < 0 || number2 < 0) {
+        return "ERROR";
+    }
+    if (number1 !== parseInt(number1) || number2 !== parseInt(number2))  {
+        return "ERROR";
+    }
 
+    let sum = 0;
+    let from;
+    let to;
+    if (number1 > number2) {
+        from = number2;
+        to = number1;
+    } else if (number2 > number1) {
+        from = number1;
+        to = number2;
+    } else {
+        from = number1;
+        to = number2;
+    }
+    for (let i = from; i <= to; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
