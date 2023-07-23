f0a49e43543e89822dd450283293c4ed1f2c882a
UdYnamite <89peterj89@gmail.com>
2023-07-23 15:10:04 +0700

    09-palindromes Done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (string = "") {
+    cleanString = string.replace(/[^\w]|_/g, "");
+    cleanLowerString = cleanString.toLowerCase();
+    let reverse = "";
+    for (let index = cleanString.length - 1; index >= 0; index--) {
+        reverse += cleanLowerString[index];
+    }
+    return cleanLowerString == reverse;
 };
 
 // Do not edit below this line
