67bde30e68e81fb3b9ca671c407b39e31c731e5c
UdYnamite <89peterj89@gmail.com>
2023-07-23 14:27:28 +0700

    08_calculator Done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((sum, value) => (sum += value), 0);
 };
 
-const multiply = function() {
-
+const multiply = function(...numbers) {
+  return numbers.reduce((result, value) => (result * value), 1);
 };
 
-const power = function() {
-	
+const power = function(number, power) {
+  let result = number;
+	for (let i = 1; i < power; i++) {
+    result *= number;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  let result = 1;
+  if (number > 0) {
+    result = number
+  }
+	for (let i = number - 1; i > 0; i--){
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
