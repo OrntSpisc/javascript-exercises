421ee6167c2d7a020517d924ef9fd910b6d9c36a
UdYnamite <89peterj89@gmail.com>
2023-07-24 11:21:12 +0700

    10_fibonacci Done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    const n = parseInt(number);
+    if (n === NaN || n < 0) {
+        return "OOPS";
+    }
 
+    let prevNum = 0;
+    let curNum = 1;
+    let helper = curNum;
+    for (let i = 1; i < n; i++) {
+        helper = curNum;
+        curNum += prevNum;
+        prevNum = helper;
+        console.log(curNum);
+    }
+    console.log(curNum);
+    return curNum;
 };
-
+fibonacci(4);
 // Do not edit below this line
 module.exports = fibonacci;
