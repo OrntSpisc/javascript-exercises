ee1573f6d082798a35c1d47b0afb7b53ec007e9d
UdYnamite <89peterj89@gmail.com>
2023-07-24 13:54:14 +0700

    12_findTheOldest Done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    return array.reduce(
+      (oldest, currentObject) => 
+      getAge(currentObject) > getAge(oldest) ? currentObject : oldest);
 };
 
+function getAge(object) {
+  const birthYear = object.yearOfBirth;
+  let deathYear = object.yearOfDeath;
+  if (!deathYear) {
+    deathYear = (new Date().getFullYear());
+  }
+  return deathYear - birthYear;
+}
 // Do not edit below this line
 module.exports = findTheOldest;
